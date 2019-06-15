e19bfcc7f812d8132ea999480e97c10954e95f80
unknown <thilanga252deshan@gmail.com>
2019-06-15 11:06:55 +0530

    init version have to implement ...

---
+++ test not cam move/mobilenet_ssd_python.py
@@ -0,0 +1,98 @@
+#Import the neccesary libraries
+import numpy as np
+import argparse
+import cv2 
+
+# construct the argument parse
+
+parser = argparse.ArgumentParser(
+    description='Script to run MobileNet-SSD object detection network ')
+parser.add_argument("--video", help="path to video file. If empty, camera's stream will be used")
+
+#-------------------------------------------------------
+
+prototxt = "MobileNetSSD_deploy.prototxt"
+weights = "MobileNetSSD_deploy.caffemodel"
+
+thr = 0.7
+
+left = 0
+right = 150
+#-------------------------------------------------------
+
+args = parser.parse_args()
+
+# Labels of Network.
+classNames = { 15: 'person' }
+
+# Open video file or capture device. 
+if args.video:
+    cap = cv2.VideoCapture(args.video)
+else:
+    cap = cv2.VideoCapture(0)
+
+#Load the Caffe model 
+net = cv2.dnn.readNetFromCaffe(prototxt, weights)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    frame_resized = cv2.resize(frame,(300,300)) # resize frame for prediction
+
+    # MobileNet requires fixed dimensions for input image(s)
+    # so we have to ensure that it is resized to 300x300 pixels.
+    # set a scale factor to image because network the objects has differents size. 
+    # We perform a mean subtraction (127.5, 127.5, 127.5) to normalize the input;
+    # after executing this command our "blob" now has the shape:
+    # (1, 3, 300, 300)
+    blob = cv2.dnn.blobFromImage(frame_resized, 0.007843, (300, 300), (127.5, 127.5, 127.5), False)
+    #Set to network the input blob 
+    net.setInput(blob)
+    #Prediction of network
+    detections = net.forward()
+
+    #Size of frame resize (300x300)
+    cols = frame_resized.shape[1] 
+    rows = frame_resized.shape[0]
+
+    #For get the class and location of object detected, 
+    # There is a fix index for class, location and confidence
+    # value in @detections array .
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2] #Confidence of prediction 
+        if confidence > thr: # Filter prediction 
+            class_id = int(detections[0, 0, i, 1]) # Class label
+
+            # Object location 
+            xLeftBottom = int(detections[0, 0, i, 3] * cols) 
+            yLeftBottom = int(detections[0, 0, i, 4] * rows)
+            xRightTop   = int(detections[0, 0, i, 5] * cols)
+            yRightTop   = int(detections[0, 0, i, 6] * rows)
+            
+            # Factor for scale to original size of frame
+            heightFactor = frame.shape[0]/300.0  
+            widthFactor = frame.shape[1]/300.0 
+            # Scale object detection to frame
+            xLeftBottom = int(widthFactor * xLeftBottom) 
+            yLeftBottom = int(heightFactor * yLeftBottom)
+            xRightTop   = int(widthFactor * xRightTop)
+            yRightTop   = int(heightFactor * yRightTop)
+            # Draw location of object  
+            cv2.rectangle(frame, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),
+                          (0, 255, 0))
+
+#-----my codes-------------------------------------------------------------------------------
+            if xLeftBottom < left:
+                left = xLeftBottom
+            elif xLeftBottom > right:
+                right = xLeftBottom
+
+            print(left, right)
+
+            
+            
+
+    cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) >= 0:  # Break with ESC 
+        break
